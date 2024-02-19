6e4c4c330135c8f16a02749dfff67f891ce843d5
u2260774 <126501906+u2260774@users.noreply.github.com>
2024-02-19 23:12:20 +0000

    Add files via upload

---
+++ mediscore_flask/app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template,request, redirect, url_for,flash
+import pickle
+from flask_wtf import FlaskForm
+from wtforms import SelectField
+from forms import InputForm
+from mediscore_function import calculate_medi_score
+
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'Eight-Handled Sword Divergent Sila Divine General Mahoraga'
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+
+    print_mediscore = False
+
+    form = InputForm()
+
+    if form.validate_on_submit():
+        resp_type = int(form.resp_type.data)
+        conc = int(form.conc.data)
+        resp_rate = form.resp_rate.data
+        time_since = form.time_since.data
+        spo2 = form.spo2.data
+        temp = form.temp.data
+        cbg = form.cbg.data
+
+        medi_score = calculate_medi_score(resp_type, conc, resp_rate, spo2, temp, cbg, time_since)
+        alert = False
+        if isinstance(medi_score[0],int):
+            alert = medi_score[1]
+            medi_score = "The patient's Medi score is "+str(medi_score[0])+"."
+        print_mediscore = True
+
+        return render_template('home.html', form=form,flag=print_mediscore,medi_score=medi_score,alert=alert)
+
+    return render_template('home.html', form=form)
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
---
+++ mediscore_flask/forms.py
@@ -0,0 +1,14 @@
+from flask_wtf import FlaskForm
+from wtforms import SelectField, IntegerField, FloatField, SubmitField
+from wtforms.validators import InputRequired
+
+class InputForm(FlaskForm):
+    resp_type = SelectField('What is the respiration type?', choices=[('', 'Please select'), ('2', 'Oxygen'), ('0', 'Air')], validators=[InputRequired()])
+    conc = SelectField('Is the patient conscious?', choices=[('', 'Please select'), ('0', 'Aware'), ('3', 'Unconscious')], validators=[InputRequired()])
+    resp_rate = IntegerField('Respiration Rate', validators=[InputRequired()])
+    time_since = IntegerField('How many hours since last meal', validators=[InputRequired()])
+    spo2 = IntegerField('Oxygen Saturation (0-100)', validators=[InputRequired()])
+    temp = FloatField("Patient's Body Temperature", validators=[InputRequired()])
+    cbg = FloatField("Patient's Blood Glucose", validators=[InputRequired()])
+
+    submit = SubmitField("Submit")
